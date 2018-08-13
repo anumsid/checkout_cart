44d50f8bdf6bf6f081ae80a2b5e879f4677d5cbc
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-08-12 21:59:03 -0400

    set up components, added bootstrap

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import {Grid} from 'react-bootstrap';
+import SubTotal from './components/Subtotal/Subtotal';
+import PickupSavings from './components/PickupSavings/PickupSavings'
+import TaxesFees from './components/TaxesFees/TaxesFees'
+ import './App.css';
+
+class App extends Component {
+   constructor(props){
+     super(props);
+
+     this.state = {
+       total: 100,
+       PickupSavings: -3.85,
+       taxes: 0
+     };
+   }
+  render() {
+    return (
+      <div className="container">
+        <Grid className="purchase-card">
+          <SubTotal price={this.state.total.toFixed(2)}/>
+          <PickupSavings price={this.state.PickupSavings}/>
+          <TaxesFees taxes={this.state.taxes.toFixed(2)}/>
+        </Grid>
+
+      </div>
+    );
+  }
+}
+
+export default App;
