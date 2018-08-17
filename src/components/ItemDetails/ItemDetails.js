16a3a5b82b7a5653e2fb5fd49da8e1c5f0a0377d
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-08-17 02:43:02 -0400

    added image tag and styling

--- src/components/ItemDetails/ItemDetails.js
+++ src/components/ItemDetails/ItemDetails.js
@@ -20,6 +20,22 @@ export default class ItemDetails extends Component {
           {this.state.open === false ? `See` : `Hide ` } item details
           {this.state.open === false ? ` +` : ` -` }
         </Button>
+        <Collapse in={this.state.open}>
+          <div>
+            <Well>
+              <Media>
+
+                  <img
+                    width={100}
+                    height={130}
+                    alt="thumbnail"
+                    src="https://i5.walmartimages.com/asr/99382568-e98b-4a27-b4dd-6705f43bde1f_1.01ed919ba7555419709fdbdf1b8fb930.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff"
+                  />
+
+              </Media>
+            </Well>
+          </div>
+        </Collapse>
       </div>
     )
   }
