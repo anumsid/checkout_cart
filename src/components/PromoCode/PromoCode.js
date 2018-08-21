cfc351503a97778d1d83483820cc8db00ed54a8f
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-08-21 00:25:08 -0400

    added css to promo field

--- src/components/PromoCode/PromoCode.js
+++ src/components/PromoCode/PromoCode.js
@@ -33,8 +33,39 @@ class PromoCodeDiscount extends Component {
         promo code
         {this.state.open === false ? ` +` : ` -`}
         </Button>
+      <Collapse in={this.state.open}>
+        <div>
+          <Well>
+            <Row className="show-grid">
+              <Col md={12}>
+                <Form>
+                  <FormGroup controlId="formInlineName">
+                    <ControlLabel> Promo Code </ControlLabel>
+                    <FormControl
+                      type="text"
+                      placeholder="Enter promo code"
+                      value={this.props.promoCode}
+                      onChange={this.handleChange}
+                    />
+                  </FormGroup>
+                  <Button
+                    block
+                    bsStyle="success"
+                    className="btn-round"
+                    disabled={this.props.isDiabled}
+                    onClick={this.props.giveDiscount}
+                  >
+                  Apply
+                </Button>
+                </Form>
+              </Col>
+            </Row>
+          </Well>
+
+        </div>
+      </Collapse>
       </div>
-    )
+    );
   }
 }
 
